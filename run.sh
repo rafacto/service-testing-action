b4fad5840f6ba732052b59f358134282178cc57f
rafacto <rafacto@gmail.com>
2021-11-24 16:52:45 -0300

    Add verification if environment is empty

--- run.sh
+++ run.sh
@@ -6,11 +6,10 @@ npm install -g newman-reporter-htmlextra
 
 mkdir -p testResults
 
-# verifica se o environment não foi passado
+# verifica se o environment não foi passado (is empty)
 if [ -z "$INPUT_ENVIRONMENT" ] ; then
-#  newman run $INPUT_COLLECTION 
-#  newman run $INPUT_COLLECTION -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
-  echo "environment is empty"
+  newman run $INPUT_COLLECTION 
+  newman run $INPUT_COLLECTION -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
 else
   newman run $INPUT_COLLECTION -e $INPUT_ENVIRONMENT 
   newman run $INPUT_COLLECTION -e $INPUT_ENVIRONMENT -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
