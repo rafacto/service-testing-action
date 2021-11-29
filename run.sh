6189e3c7cb519f6977644330731b3c8e8b25b866
Rafael Santos <33627565+rafacto@users.noreply.github.com>
2021-11-29 15:09:41 -0300

    Add verrification pull request to brench releases

--- run.sh
+++ run.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 
-npm install -g newman
+# verifica se o evento que disparou a branch é um pull_request para alguma branch de release
+if [ $GITHUB_BASE_REF = "release/*" ] ; then
+  npm install -g newman
 
-npm install -g newman-reporter-htmlextra
+  npm install -g newman-reporter-htmlextra
 
-mkdir -p testResults
+  mkdir -p testResults
 
-echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
-echo "GITHUB_REF: $GITHUB_REF"
-echo "GITHUB_REF_NAME: $GITHUB_REF_NAME"
-echo "GITHUB_HEAD_REF: $GITHUB_HEAD_REF"
-echo "GITHUB_BASE_REF: $GITHUB_BASE_REF"
+  # verifica se o environment não foi passado (is empty)
+  if [ -z "$INPUT_ENVIRONMENTPATH" ] ; then
+    newman run $INPUT_COLLECTIONPATH
+    newman run $INPUT_COLLECTIONPATH -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
+  else
+    newman run $INPUT_COLLECTIONPATH -e $INPUT_ENVIRONMENTPATH 
+    newman run $INPUT_COLLECTIONPATH -e $INPUT_ENVIRONMENTPATH -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
+  fi
 
-# verifica se o environment não foi passado (is empty)
-if [ -z "$INPUT_ENVIRONMENTPATH" ] ; then
-  newman run $INPUT_COLLECTIONPATH
-  newman run $INPUT_COLLECTIONPATH -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
+  # armazena o status (0: sucesso, 1: falhou) do último comando executado (o newman)
+  testFailed=$?
+
+  testReportPath="testResults/htmlreport.html"
+  echo "::set-output name=testReportPath::$testReportPath"
+
+  # se os testes falharam, sai com código 1, o que vai parar a esteira
+  #if [ $testFailed -eq 1 ] ; then
+  #  echo "Game over!"
+  #  exit 1
+  #fi
+
+  cd testResults
+  for entry in "$PWD"/*
+  do
+    echo "$entry"
+  done
+  
 else
-  newman run $INPUT_COLLECTIONPATH -e $INPUT_ENVIRONMENTPATH 
-  newman run $INPUT_COLLECTIONPATH -e $INPUT_ENVIRONMENTPATH -r htmlextra --reporter-htmlextra-export testResults/htmlreport.html
+  echo "Service tests were not performed. They are executed only for pull requests to release branches."
 fi
 
-# armazena o status (0: sucesso, 1: falhou) do último comando executado (o newman)
-testFailed=$?
 
-testReportPath="testResults/htmlreport.html"
-echo "::set-output name=testReportPath::$testReportPath"
-
-# se os testes falharam, sai com código 1, o que vai parar a esteira
-#if [ $testFailed -eq 1 ] ; then
-#  echo "Game over!"
-#  exit 1
-#fi
-
-cd testResults
-for entry in "$PWD"/*
-do
-  echo "$entry"
-done
